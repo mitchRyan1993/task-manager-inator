c50f329f39c10583e6786e0036246bfcd3a54505
mitchRyan1993 <90662669+mitchRyan1993@users.noreply.github.com>
2026-03-23 20:32:43 -0400

    Added Class Model

---
+++ ClassModel/TaskItem.cs
@@ -0,0 +1,32 @@
+﻿namespace TMI_API.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public Status Status { get; set; }
+        public Priority Priority { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsComplete { get; set; }
+    }
+
+    public enum Status
+    {
+        Pending,
+        InProgress,
+        Blocked,
+        Rejected,
+        Completed,
+        Archived
+    }
+
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High,
+        Critical
+    }
+}
---
+++ ClassModel/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TMI_API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string PasswordHash { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime Created { get; set; }
+        public Role Role { get; set; }
+    }
+
+    public enum Role
+    {
+        Customer,
+        Admininistrator,
+    }
+}
